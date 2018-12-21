f61ff6e845d48842e59eca5e785c2400e22a51ab
Bernardo Suarez Sepulveda <b@xhash.tech>
2018-12-20 18:40:06 -0600

    Created separate file for views, fixed first query, improved README file

---
+++ create_views.sql
@@ -0,0 +1,24 @@
+ˆcreate view question1 as select '"'||articles.title||'"' as title, count(*) as views
+     from articles, log 
+     WHERE log.path = CONCAT('/article/', articles.slug) 
+     group by articles.title 
+     order by views desc
+     limit 3;
+
+create view question2 as select authors.name as author, count (*) as views 
+     from articles, authors, log 
+     where authors.id::text=articles.author::text 
+     and log.path like '%'||articles.slug||'%' 
+     group by authors.name 
+     order by views desc;
+
+
+create view question3 as select to_char(to_date(tabla1.fecha::text,'YYYY-MM-DD'),'Mon DD YYYY'), round(100.0*tabla1.errores/tabla2.totales,1)||'%' as porcentajes
+     from (select time::date as fecha, count(*) as errores
+         from log where status!='200 OK'
+         group by time::date)
+         as tabla1
+     join (select log.time::date as fecha, count (*) as totales
+         from log group by log.time::date)
+         as tabla2
+     on tabla1.fecha=tabla2.fecha where 100.0*tabla1.errores/tabla2.totales>1;
---
+++ reporting_tool.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import psycopg2
+
+def connect(news):
+    """
+     Connect to the PostgreSQL database. 
+     Return a database connection.
+    """
+    try:
+        db = psycopg2.connect("dbname={}".format(news))
+    except psycopg2.Error as e:
+        print("Unable to connect!")
+        print(e.pgerror)
+        print(e.diag.message_detail)
+        sys.exit(1)    
+    else:
+        print("\n"+"Connected! Performing query")
+    cursor = db.cursor()
+    return db, cursor
+
+def busqueda(pregunta, titulo, unidad):
+    conn, cursor = connect("news")
+    if __name__=='__main__':
+        cursor.execute("select * from "+pregunta)
+        results = cursor.fetchall()
+        formated_results = "\n".join(
+            [str(result[0])+" - "+str(result[1])+unidad for result in results])
+        print("\n"+(titulo))
+        print(formated_results)
+        conn.close()
+    else:
+        print ("Import")
+
+
+get_popular_articles = busqueda(
+    'question1', 'Most popular articles: ', ' views')
+get_popular_authors = busqueda(
+    'question2', 'Most popular authors: ', ' views')
+get_buggy_days = busqueda(
+    'question3', 'Buggy days: ', ' errors')
